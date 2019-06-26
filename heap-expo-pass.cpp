d09d160c25e86d3811cbced25387f0f3a5978f62
Zekun Shen <17345356+buszk@users.noreply.github.com>
2019-06-26 22:20:24 +0000

    all to module pass

--- heap-expo-pass.cpp
+++ heap-expo-pass.cpp
@@ -152,104 +152,23 @@ static bool isConstantGlobalPtr(Value *V) {
     return false;
 }
 
-struct HeapExpoGlobalTracker : public ModulePass {
+struct HeapExpo : public ModulePass {
     static char ID;
 
     Function *GlobalHookFunc;
-    bool initialized;
     const DataLayout *DL;
     Module *M;
-
-    HeapExpoGlobalTracker() : ModulePass(ID) { initialized = false; }
-
-
-    virtual bool runOnModule(Module &Mod) {
-        size_t global_instr_cnt = 0;
-        M = &Mod;
-        if (!initialized)
-            doInitialization(Mod);
-
-        std::string CtorFuncName = "init_global_vars_" + M->getModuleIdentifier();
-
-        Function *CtorFunc = (Function*) M->getOrInsertFunction(CtorFuncName, VoidTy(M));
-
-        addToGlobalCtors(M, CtorFunc);
-
-        BasicBlock *B = BasicBlock::Create(M->getContext(), "entry", CtorFunc);
-        IRBuilder<> Builder(B);
-
-
-        for (auto &global: M->globals()) {
-            GlobalValue *G = &global;
-            if (!isa<GlobalVariable>(G)) 
-                continue;
-            GlobalVariable *GV = (GlobalVariable*) G;
-
-            if (G->getName() == "llvm.global_ctors" ||
-                G->getName() == "llvm.global_dtors" ||
-                G->getName() == "llvm.global.annotations" ||
-                G->getName() == "llvm.used") 
-                continue;
     
-            /* Ignore constant global variables */
-            if (GV->isConstant())
-                continue;
-
-            size_t elementSize = DL->getTypeAllocSize(G->getType()->getPointerElementType());
-            std::vector<Value*> Args;
-            Value *ptr = Builder.CreatePtrToInt(G, Int8PtrTy(M));
-            Args.push_back(ptr);
-            Value *size = ConstantInt::get(SizeTy(M), elementSize);
-            Args.push_back(size);
-            Builder.CreateCall(GlobalHookFunc, Args);
-            global_instr_cnt ++;
-        }
-        
-        Builder.CreateRet(NULL);
-        
-        std::ostringstream ss;
-        ss << "Instrumented tracking to " << global_instr_cnt << 
-            " global variables\n";
-        LOG(LVL_INFO) << ss.str();
-        return false;
-    }
-
-    virtual bool doInitialization(Module &Mod) { 
-        M = &Mod;
-        DL = &(M->getDataLayout());
-        if (!DL)
-            LOG(LVL_ERROR) << "Data Layout required\n";
-
-        GlobalHookFunc = (Function*)M->getOrInsertFunction("global_hook", VoidTy(M), Int8PtrTy(M), SizeTy(M));
-        initialized = true;
-        return false;
-    }
-
-};
-struct HeapExpo : public FunctionPass {
-    static char ID;
-    bool initialized;
     Function *regptr, *deregptr;
-    Module *M;
     Function *func;
     size_t store_instr_cnt = 0;
     size_t stack_store_instr_cnt = 0;
     size_t store_const_global_cnt = 0;
-
-    HeapExpo() : FunctionPass(ID) { initialized = false; }
-
-    void initialize() {
-        Constant *deregptr_def = M->getOrInsertFunction("deregptr", VoidTy(M), Int8PtrTy(M));
-        deregptr = cast<Function>(deregptr_def);
-        deregptr->setCallingConv(CallingConv::C);
         
-        Constant *regptr_def = M->getOrInsertFunction("regptr", VoidTy(M), Int8PtrTy(M), Int8PtrTy(M));
-        regptr = cast<Function>(regptr_def);
-        regptr->setCallingConv(CallingConv::C);
+    bool initialized = true;
 
-        initialized = true;
+    HeapExpo() : ModulePass(ID) {  }
 
-    }
 
     void instrDereg(StoreInst *SI) {
 
@@ -290,60 +209,101 @@ struct HeapExpo : public FunctionPass {
         regptr_call->setDebugLoc(DLoc);
     }
 
-    bool runOnFunction(Function &F) override {
-//        if (F.getName() == "regptr") return false;
-//        if (F.getName() == "deregptr") return false;
-        LOG(LVL_DEBUG) << "HeapExpo: ";
-        LOG(LVL_DEBUG).write_escaped(demangleName(F.getName())) << '\n';
-        M = F.getParent();
-        func = &F;
-        if (!initialized) 
-            initialize();
-        for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; i++) {
-            Instruction *I = &*i;
-            if (isa<StoreInst> (I)) {
-                LOG(LVL_DEBUG) << "Store instruction: " << *I << "\n";
-                StoreInst *SI = dyn_cast<StoreInst> (I);
-                if (SI->getValueOperand()->getType()->isPointerTy()) {
-                    if (isa<GlobalVariable>(SI->getPointerOperand()))
-                        LOG(LVL_DEBUG) << "Storing to global var\n" ;
-                    if (isa<ConstantPointerNull>(SI->getValueOperand())) {
-                        LOG(LVL_DEBUG) << "Value is a nullptr\n";
-                            
-                        instrDereg(SI);
-                        
-                        store_instr_cnt ++;
+    virtual bool runOnModule(Module &Mod) {
+        size_t global_instr_cnt = 0;
+        M = &Mod;
+        
+        doInitialization(Mod);
 
-                    } else {
-                        LOG(LVL_DEBUG) << "Value is a ptr\n";
-                        
-                        /* Don't instr if storing to stack */
-                        if (isStackPtr(SI->getPointerOperand())){
-                            stack_store_instr_cnt++;
-                            continue;
-                        }
+        std::string CtorFuncName = "init_global_vars_" + M->getModuleIdentifier();
 
-                        //if (isConstantGlobalPtr(SI->getValueOperand())) {
-                        if (isa<Constant>(SI->getValueOperand())) {
+        Function *CtorFunc = (Function*) M->getOrInsertFunction(CtorFuncName, VoidTy(M));
+
+        addToGlobalCtors(M, CtorFunc);
+
+        BasicBlock *B = BasicBlock::Create(M->getContext(), "entry", CtorFunc);
+        IRBuilder<> Builder(B);
+
+
+        for (auto &global: M->globals()) {
+            GlobalValue *G = &global;
+            if (!isa<GlobalVariable>(G)) 
+                continue;
+            GlobalVariable *GV = (GlobalVariable*) G;
+
+            if (G->getName() == "llvm.global_ctors" ||
+                G->getName() == "llvm.global_dtors" ||
+                G->getName() == "llvm.global.annotations" ||
+                G->getName() == "llvm.used") 
+                continue;
+    
+            /* Ignore constant global variables */
+            if (GV->isConstant())
+                continue;
+
+            size_t elementSize = DL->getTypeAllocSize(G->getType()->getPointerElementType());
+            std::vector<Value*> Args;
+            Value *ptr = Builder.CreatePtrToInt(G, Int8PtrTy(M));
+            Args.push_back(ptr);
+            Value *size = ConstantInt::get(SizeTy(M), elementSize);
+            Args.push_back(size);
+            Builder.CreateCall(GlobalHookFunc, Args);
+            global_instr_cnt ++;
+        }
+        
+        Builder.CreateRet(NULL);
+        
+
+        for (auto &F : *M) {
+            LOG(LVL_DEBUG) << "HeapExpo: ";
+            LOG(LVL_DEBUG).write_escaped(demangleName(F.getName())) << '\n';
+            M = F.getParent();
+            func = &F;
+            for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; i++) {
+                Instruction *I = &*i;
+                if (isa<StoreInst> (I)) {
+                    LOG(LVL_DEBUG) << "Store instruction: " << *I << "\n";
+                    StoreInst *SI = dyn_cast<StoreInst> (I);
+                    if (SI->getValueOperand()->getType()->isPointerTy()) {
+                        if (isa<GlobalVariable>(SI->getPointerOperand()))
+                            LOG(LVL_DEBUG) << "Storing to global var\n" ;
+                        if (isa<ConstantPointerNull>(SI->getValueOperand())) {
+                            LOG(LVL_DEBUG) << "Value is a nullptr\n";
+                                
                             instrDereg(SI);
-                            store_const_global_cnt++;
-                            continue;
+                            
+                            store_instr_cnt ++;
+
+                        } else {
+                            LOG(LVL_DEBUG) << "Value is a ptr\n";
+                            
+                            /* Don't instr if storing to stack */
+                            if (isStackPtr(SI->getPointerOperand())) {
+                                stack_store_instr_cnt++;
+                                continue;
+                            }
+
+                            //if (isConstantGlobalPtr(SI->getValueOperand())) 
+                            if (isa<Constant>(SI->getValueOperand())) {
+                                instrDereg(SI);
+                                store_const_global_cnt++;
+                                continue;
+                            }
+
+                            instrReg(SI);
+
+                            store_instr_cnt ++;
+
                         }
-
-                        instrReg(SI);
-
-                        store_instr_cnt ++;
 
                     }
-
                 }
             }
         }
 
-        return false;
-    }
-    bool doFinalization(Module &Mod) override {
         std::ostringstream ss;
+        ss << "Instrumented tracking to " << global_instr_cnt << 
+            " global variables\n";
         ss << "Instrumented tracking to " << store_instr_cnt << 
             " all store instructions\n";
         ss << "Instrumented tracking to " << stack_store_instr_cnt << 
@@ -354,25 +314,36 @@ struct HeapExpo : public FunctionPass {
         return false;
     }
 
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-        AU.setPreservesCFG();
+    virtual bool doInitialization(Module &Mod) { 
+        M = &Mod;
+        DL = &(M->getDataLayout());
+        if (!DL)
+            LOG(LVL_ERROR) << "Data Layout required\n";
+
+        GlobalHookFunc = (Function*)M->getOrInsertFunction("global_hook", VoidTy(M), Int8PtrTy(M), SizeTy(M));
+        Constant *deregptr_def = M->getOrInsertFunction("deregptr", VoidTy(M), Int8PtrTy(M));
+        deregptr = cast<Function>(deregptr_def);
+        deregptr->setCallingConv(CallingConv::C);
+        
+        Constant *regptr_def = M->getOrInsertFunction("regptr", VoidTy(M), Int8PtrTy(M), Int8PtrTy(M));
+        regptr = cast<Function>(regptr_def);
+        regptr->setCallingConv(CallingConv::C);
+        
+        initialized = true;
+        return false;
     }
-}; // end of struct HeapExpo
-}  // end of anonymous namespace
+
+};
+}
 
 char HeapExpo::ID = 0;
 static RegisterPass<HeapExpo> X("HeapExpo", "HeapExpo Func Pass",
                              false /* Only looks at CFG */,
                              false /* Analysis Pass */);
-char HeapExpoGlobalTracker::ID = 1;
-static RegisterPass<HeapExpoGlobalTracker> Y("HeapExpoGlobal", "HeapExpo Module Pass",
-                                            false,
-                                            false);
 
 static void registerMyPass(const PassManagerBuilder &,
                            legacy::PassManagerBase &PM) {
     PM.add(new HeapExpo());
-    PM.add(new HeapExpoGlobalTracker());
 }
 
 static RegisterStandardPasses
